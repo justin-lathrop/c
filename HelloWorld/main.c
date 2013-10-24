7ad021ea3a5d6001c21b85867c33bd169039f57f
jelathro <jelathrop@gmail.com>
2013-10-24 19:48:24 -0400

    Refactored directory layout, removed unneeded files/folders.

---
+++ HelloWorld/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * @author jelathro
+ * @date 2/18/13
+ * 
+ * Print "Hello, World!" to the console
+ */
+int main(void){
+    printf("Hello, World!\n");
+    return 0;
+}
