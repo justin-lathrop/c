dde0a2d99cd62dccf9463c097e1d868c004098e5
Justin Lathrop <jelathrop@gmail.com>
2015-08-27 21:06:58 -0400

    updating the inputLine function to use 'getdelim' POSIX function

--- input_output/StringInput.c
+++ input_output/StringInput.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define BUF_SIZE 1024
 
@@ -9,19 +10,38 @@
  * Taking input without overflow of buffer
  * (I/O) in C.
  */
- char* inputLine(size_t, char);
- int main(void){
+
+char * inputLine(size_t, char);
+
+int main(void){
     printf("Enter a string: ");
+
     char* line = inputLine(BUF_SIZE,'\n');
     printf("You Entered: %s",line);
-    return 0;
- }
-char* inputLine(size_t size, char delimiter)
-{
+
+    free(line);
+    return(0);
+}
+
+char * inputLine(size_t size, char delimeter){
+    char * buffer = NULL;
+    size_t read;
+
+    read = getdelim(&buffer, &size, delimeter, stdin);
+    if(-1 != read){
+        return(buffer);
+    }else{
+        free(buffer);
+        return(NULL);
+    }
+}
+
+/*
+char* inputLine(size_t size, char delimiter){
     char* str;
     int ch;
     size_t len = 0;
-    str = realloc(NULL, sizeof(char) * size);	//size is start size
+    str = malloc(NULL, sizeof(char) * size);
     if(!str)
     	return str;
 
@@ -34,13 +54,17 @@ char* inputLine(size_t size, char delimiter)
             if(!str)return str;
         }
     }
+
     if(ch == EOF && len == 0)
     {
         // Ability to check EOF easily
     	str[0] = EOF;
     	return str;
     }
+
     // Adding null at the end
     str[len++]='\0';
     return realloc(str, sizeof(char) * len);
 }
+*/
+
