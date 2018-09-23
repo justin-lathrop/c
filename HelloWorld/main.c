26e165b4676077c87bd9ea66b06f48c038831294
root <abdou.developer@gmail.com>
2018-09-23 17:52:39 +0100

    Replaced printf function to our own function

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,12 +1,36 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /*
  * @author jelathro
  * @date 2/18/13
  * 
+ * @reviewd by Abdou Developer (Abdelhakim R)
+ * @https://github.com/test20091
+ * @date 09/23/2018
+ *
  * Print "Hello, World!" to the console
+ * fputstr "Hello, World!" using native function write
+ * and pointer to char
  */
-int main(void){
-    printf("Hello, World!\n");
+
+void	fputchar (char c)
+{
+	write (1, &c, 1);
+}
+
+void	fputstr (char *str)
+{
+	char c;
+
+	while (*str)
+	{
+		c = *str;
+		fputchar(c);
+		str++;
+	}
+}
+
+int	main(void){
+    fputstr("Hello, World!\n");
     return 0;
 }
