2269611c7d330dc51a9411c57713193ee10a3c17
Rohit Patil <32801968+RohitPatilRRP@users.noreply.github.com>
2018-10-01 23:40:23 +0530

    simple

--- input_output/main.c
+++ input_output/main.c
@@ -1,30 +1,9 @@
 #include <stdio.h>
-
-#define BUF_SIZE 1024
-
-/*
- * @author jelathro
- * @data 3/19/13
- * 
- * Show some of the various input output
- * (I/O) methods in C.
- */
-int main(void){
-    // Input Output using printf
-    // and scanf
-    int number_entered = 0;
-    printf("number_entered = %d\n", number_entered);
-    scanf("%d", &number_entered);
-    printf("number_entered = %d\n", number_entered);
-    
-    // Input Output using STDIN and
-    // STDOUT
-    char buffer[BUF_SIZE];
-    printf("Entering STDIN while loop\n");
-    while(fgets(buffer, BUF_SIZE, stdin)){
-        puts(buffer);
-    }
-    printf("You pressed control+d, so i'll leave now\n");
-
+int main()
+{
+    int testInteger;
+    printf("Enter an integer: ");
+    scanf("%d",&testInteger);  
+    printf("Number = %d",testInteger);
     return 0;
 }
