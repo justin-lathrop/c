00a83b96f59ae4c4e8242bbb0f1a7660dabe33ec
Marzooq13579 <marzooqabs@gmail.com>
2018-10-28 00:44:22 +0530

    added extra statement

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -3,10 +3,11 @@
 /*
  * @author jelathro
  * @date 2/18/13
- * 
+ *
  * Print "Hello, World!" to the console
  */
 int main(void){
     printf("Hello, World!\n");
+    printf("Share your smile with everyone");
     return 0;
 }
