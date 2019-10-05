a6dc18c0c70867184adfde0a4e6abce2f4e0d277
Natasha2001 <natashasee.2001@gmail.com>
2019-10-05 15:42:35 +0530

    Add indentation to program

--- input_output/main.c
+++ input_output/main.c
@@ -9,22 +9,21 @@
  * Show some of the various input output
  * (I/O) methods in C.
  */
-int main(void){
-    // Input Output using printf
-    // and scanf
-    int number_entered = 0;
-    printf("number_entered = %d\n", number_entered);
-    scanf("%d", &number_entered);
-    printf("number_entered = %d\n", number_entered);
+int main(void)
+{
+    	// Input Output using printf and scanf
+    	int number_entered = 0;
+    	printf("number_entered = %d\n", number_entered);
+    	scanf("%d", &number_entered);
+    	printf("number_entered = %d\n", number_entered);
     
-    // Input Output using STDIN and
-    // STDOUT
-    char buffer[BUF_SIZE];
-    printf("Entering STDIN while loop\n");
-    while(fgets(buffer, BUF_SIZE, stdin)){
-        puts(buffer);
-    }
-    printf("You pressed control+d, so i'll leave now\n");
-
-    return 0;
+    	// Input Output using STDIN and STDOUT
+    	char buffer[BUF_SIZE];
+    	printf("Entering STDIN while loop\n");
+    	while(fgets(buffer, BUF_SIZE, stdin))
+	{
+        	puts(buffer);
+    	}
+   	printf("You pressed control+d, so i'll leave now\n");
+	return 0;
 }
