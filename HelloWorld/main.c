f8c8fc8e9c336f6ee515088314b6e2639a06f134
chandresh189 <48803866+chandresh189@users.noreply.github.com>
2019-10-12 10:37:26 +0530

    Update main.c

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -6,7 +6,9 @@
  * 
  * Print "Hello, World!" to the console
  */
+//int before main implies that the main function return integer
 int main(void){
+    //main function is called by operating system
     printf("Hello, World!\n");
     return 0;
 }
