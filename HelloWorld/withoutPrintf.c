0c511f3f12cb2e4ec229b70a353b42a5455c2241
Pratik Narola <35926958+Pratiknarola@users.noreply.github.com>
2019-10-20 22:32:00 +0530

    Create withoutPrintf.c

---
+++ HelloWorld/withoutPrintf.c
@@ -0,0 +1,16 @@
+int main (void)
+{
+    puts("Hello, World!");
+    return 0;
+}
+
+/*
+
+same as above
+
+#include <stdio.h>
+int main(void){
+  char *s="hello world\n";
+  while (*s) putchar(*s++);
+}
+*/
