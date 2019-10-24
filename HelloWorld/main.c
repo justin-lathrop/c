72a3994645ad0021f347881a8e7040a609479b1e
ansh10dave <56872112+ansh10dave@users.noreply.github.com>
2019-10-25 01:47:30 +0530

    Update main.c

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -4,9 +4,10 @@
  * @author jelathro
  * @date 2/18/13
  * 
- * Print "Hello, World!" to the console
+ * Print "Hello, World!" to the console 
  */
 int main(void){
-    printf("Hello, World!\n");
+    printf("Hello World!\n");
     return 0;
 }
+//Done
