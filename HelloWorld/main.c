c92aac031241776eafa340874cb07926ef634f83
VISHNU KT <37811932+vishnukt@users.noreply.github.com>
2019-10-26 22:03:44 +0530

    Hello World printed in different sizes and colours.
    Hello World printed in different sizes and colours.

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,12 +1,37 @@
-#include <stdio.h>
-
-/*
- * @author jelathro
- * @date 2/18/13
- * 
- * Print "Hello, World!" to the console
- */
-int main(void){
-    printf("Hello, World!\n");
-    return 0;
-}
+#include<stdio.h> 
+#include<graphics.h> 
+#include<dos.h> 
+  
+// function for printing 
+// message as colored character 
+void printMsg() 
+{ 
+    // auto detection 
+    int gdriver = DETECT,gmode,i; 
+  
+    // initialize graphics mode 
+    initgraph(&gdriver,&gmode,"C:\\Turboc3\\BGI"); 
+  
+    for (i=3; i<7; i++) 
+    { 
+        // setcolor of cursor 
+        setcolor(i); 
+          
+        // set text style as 
+        // settextstyle(font, orientation, size) 
+        settextstyle(i,0,i); 
+          
+        // print text at coordinate x,y; 
+        outtextxy(100,20*i,"Hello World"); 
+          
+        delay(500); 
+    }  
+    delay(2000); 
+} 
+  
+// driver program 
+int main() 
+{ 
+    printMsg(); 
+    return 0; 
+} 
