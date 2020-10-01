2d23410fbb127e62f23d1cecc73a1f7403693930
pkpprasun <69473118+pkpprasun@users.noreply.github.com>
2020-10-01 19:36:10 +0530

    Update main.c

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <conio.h>
 /*
  * @author jelathro
  * @date 2/18/13
  * 
- * Print "Hello, World!" to the console
+ * Print "Hello World!" to the console
  */
 int main(void){
-    printf("Hello, World!\n");
+    printf("Hello World!\n");
     return 0;
 }
