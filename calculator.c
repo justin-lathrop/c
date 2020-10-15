489ef59a214aea2dc4029639e7a94177eb618e2b
mr-rawat <72936796+mr-rawat@users.noreply.github.com>
2020-10-15 22:30:04 +0530

    create calculator.c
    This is a basic calculator program.Thank you

---
+++ calculator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+ 
+int main()
+{
+	char Operator;
+	float num1, num2, result = 0;
+	
+	printf("\n Please Enter an Operator (+, -, *, /)  :  ");
+  	scanf("%c", &Operator);
+  	
+	printf("\n Please Enter the Values for two Operands: num1 and num2  :  ");
+  	scanf("%f%f", &num1, &num2);
+  	
+  	switch(Operator)
+  	{
+  		case '+':
+  			result = num1 + num2;
+  			break;
+  		case '-':
+  			result = num1 - num2;
+  			break;  			
+  		case '*':
+  			result = num1 * num2;
+  			break;
+  		case '/':
+  			result = num1 / num2;
+  			break;
+		default:
+			printf("\n You have enetered an Invalid Operator ");				    			
+	}
+  
+	printf("\n The result of %.2f %c %.2f  = %.2f", num1, Operator, num2, result);
+	
+  	return 0;
+}
