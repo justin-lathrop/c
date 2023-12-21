7600b1aa1b7335cc0db4a3412f158b758d107722
Mohammad Sahil <144834170+mdsahilnoob@users.noreply.github.com>
2023-12-21 23:59:49 +0530

    Create binarysearch.c

---
+++ BinarySearch/binarysearch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // Check if the target is present at the middle
+        if (arr[mid] == target)
+            return mid;
+
+        // If the target is greater, ignore the left half
+        if (arr[mid] < target)
+            left = mid + 1;
+
+        // If the target is smaller, ignore the right half
+        else
+            right = mid - 1;
+    }
+
+    // If the target is not present in the array
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+
+    int result = binarySearch(arr, 0, n - 1, target);
+
+    if (result == -1)
+        printf("Element is not present in array\n");
+    else
+        printf("Element is present at index %d\n", result);
+
+    return 0;
+}
